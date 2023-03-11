b365d15f6635d3bca87233b6520edd1e9da9969d
landay <landiehan@outlook.com>
2023-03-11 13:21:45 +0800

    config blog dev environment

--- apps/blog/app/root.tsx
+++ apps/blog/app/root.tsx
@@ -1,16 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Meta, Outlet } from "@remix-run/react";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Blog | Landay",
   viewport: "width=device-width,initial-scale=1",
 });
 
@@ -19,13 +12,9 @@ export default function App() {
     <html lang="en">
       <head>
         <Meta />
-        <Links />
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
       </body>
     </html>
   );
